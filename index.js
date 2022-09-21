a8251026d93ce984f9dbf9efb558b8e6ff93e2d2
Bernard Legaspi <blegaspi26@gmail.com>
2022-09-21 13:34:32 -0400

    commit tree 103092de5e19b2529763d0af596e80b23c47cd88

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+
+//add a header to body 
+
+/*
+document.querySelector('#main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = ('victory')
+
+newHeader.textContent = ('is the champion') */
+
+
+
+
+
+document.querySelector('#main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = ('victory')
+
+newHeader.textContent = ('Bernard is the champion')
